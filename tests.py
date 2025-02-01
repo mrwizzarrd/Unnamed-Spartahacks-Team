1228e54cdc269c324724469f838424ed6dcdb320
SpacePiggy17 <xoogle17@gmail.com>
2025-02-01 17:45:55 -0500

    Updated tests file.

--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 from ocr import ocr
 from json_to_ics import json_to_ics, write_ics
+from AI import getResponse
 
 text = ocr("image.png")
 
-json_string = 
+json_string = getResponse(text).choices[0].message.content
 
 ics_data = json_to_ics(json_string)
 
-write_ics(ics_data, "calendar.ics")
+write_ics(ics_data, "calendar2")
