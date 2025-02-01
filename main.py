f53d60bf2e12a0f846e0c1314b3e45fda6e809b4
mrwizzarrd <rvermiglio865@gmail.com>
2025-02-01 17:56:26 -0500

    commiting

---
+++ main.py
@@ -0,0 +1,8 @@
+import ocr
+import AI
+import asyncio
+#import gui
+
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+print(AI.getResponseJson("testing testing"))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(AI.getResponseJson("testing testing"))
